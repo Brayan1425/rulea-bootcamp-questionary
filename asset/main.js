5ec4cfb44b1fd1a629b27e687f47ce7e3ef3f7e8
brayan <boacs14@gmail.com>
2023-03-24 08:25:21 -0500

    add changes in question 1,2,3 of the html and css

---
+++ asset/main.js
@@ -0,0 +1,8 @@
+  
+                <button onclick="triggerModal()">
+                trigger me babe
+                </button>
+                 <div class="dialog" id="dialog">
+                 Ups try again
+                 <button onclick="hideModal()">x</button>
+             </div>
